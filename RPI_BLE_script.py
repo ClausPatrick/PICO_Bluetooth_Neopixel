69ea13d80e4b1b53af66bdc76da783ba451c5d71
ClausPatrick <44665589+ClausPatrick@users.noreply.github.com>
2025-09-17 13:27:26 +0100

    Update RPI_BLE_script.py
    Added logging

--- RPI_BLE_script.py
+++ RPI_BLE_script.py
@@ -10,24 +10,14 @@ import array
 logname = str(sys.argv[0])[:-3] + '.log'
 logging.basicConfig(filename=logname,  level=logging.DEBUG, format='%(asctime)s %(levelname)s %(funcName)s:%(lineno)d %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
 
+CMD_COUNTER_LOG = "bt_ble_command_counter.log"
+CRC_POLY = 0xEDB88320
+KEY_FILE = "bt_ble_secret_key.key"
 
-try:
-    with open('bt_ble_command_counter.log', 'r') as f:
-        command_counter = json.loads(f.read())
-        #print(f'logs: {command_counter} \n')
-except FileNotFoundError:
-    command_counter = {}
-    command_list = ['AIX', 'ZIX', 'GRA', 'TSY', 'BRI', 'DAL', 'CDP', 'TST', 'ALD']
-    for c in command_list:
-        command_counter[c] = 0
-    with open('bt_ble_command_counter.log', 'w') as f:
-        json.dump(command_counter, f)
-        #print(f'Updating command_counter.log: {command_counter} \n')
-
-
-with open('bt_ble_secret_key.key', 'r') as k:
-    key =  k.read()
-    #print(f'key: {key}')
+def get_secret_key():
+    with open(KEY_FILE, 'r') as k:
+        key =  k.read().strip()
+    return key
 
 def ticks_to_time():
     now = datetime.now()
@@ -40,6 +30,7 @@ def ticks_to_time():
     return t
 
 def encrypt(cmd):
+    key = get_secret_key()
     assert isinstance(cmd, str), "Input not string"
     lenght = len(cmd)
     key_lenght = len(key)
@@ -51,39 +42,38 @@ def encrypt(cmd):
         new += ''.join(chr(b ^ key_mask))
     print(f'encrypt:: clean: {cmd},  new: {new}\n')
     return new
-
-
-
-CRC_POLY = 0xEDB88320
-CRC_table = array.array('L')
-for byte in range(256):
-    crc = 0
-    for bit in range(8):
-        if (byte ^ crc) & 1:
-            crc = (crc >> 1) ^ CRC_POLY
-        else:
-            crc >>= 1
-        byte >>= 1
-    CRC_table.append(crc)
-
+ 
+def setup_crc_table():
+    CRC_table = array.array('L')
+    for byte in range(256):
+        crc = 0
+        for bit in range(8):
+            if (byte ^ crc) & 1:
+                crc = (crc >> 1) ^ CRC_POLY
+            else:
+                crc >>= 1
+            byte >>= 1
+        CRC_table.append(crc)
+    return CRC_table
 
 def add_crc(cmd):
+    CRC_table = setup_crc_table()
     def crc32(string):
         v = 0xffffffff
         for c in string:
             v = CRC_table[(ord(c) ^ v) & 0xff] ^ (v >> 8)
-            #print(f'add_crc:: c: {c}, v: {v}')
         return -1 - v
+
     crc_val = str(hex(crc32(cmd + ':') & 0xffffffff)[2:])
     print(f'add_crc:: cmd: {cmd}, crc_val: {crc_val}, ')
-    #print(cmd + ':' + crc_val)
     return cmd + ':' + crc_val
-    #return cmd+crc_val
 
 
 def command_code_formatter(cmd):
-    #cur = command_counter[command_list[cmd]]
-    cur = command_counter[cmd]
+    try:
+        cur = int(command_counter[cmd])
+    except KeyError:
+        cur = 0
     cur += 1
     command_counter[cmd] = cur
     return cmd + ('000000' + str(cur))[-6:]
@@ -91,96 +81,10 @@ def command_code_formatter(cmd):
 
 
 def run_command():
-    if len(sys.argv) > 1:
-        command = sys.argv[1]
-        if command == '-a':
-            cmd_code = command_code_formatter('AIX')
-            start = str(sys.argv[2])
-            end = str(sys.argv[3])
-            data = str(sys.argv[4])
-            message = cmd_code + ':' + start + ':' + end + ':' + data
-        if command == '-z':
-            cmd_code = command_code_formatter('ZIX')
-            zone = str(sys.argv[2])
-            data = str(sys.argv[3])
-            message = cmd_code + ':' + zone + ':' + data
-        if command == '-g':
-            cmd_code = command_code_formatter('GRA')
-            data = ''
-            ds = sys.argv[1:]
-            for d in ds:
-                data = data + ':' + d
-                print(data)
-            message = cmd_code + data
-            print(message)
-        if command == '-g?':
-            print(f'To set gradient: -g START<0-n>* RrGgBb<000000-ffffff>* MIDDLE_n0<0-n>* RrGgBb<000000-ffffff>* MIDDLE_n1<0-n>* RrGgBb<000000-ffffff>* END<0-n>* RrGgBb<000000-ffffff>*.') 
-            print(f'Example: <-g 0 00ff00 10 ff0000 20 0000ff>. * Are required. Any number of middle points are accepted so long there are as many RrGgBb values'
-        if command == '-t':
-            cmd_code = command_code_formatter('TSY')
-            data  = ticks_to_time()
-            message = cmd_code + ':' + data
-        if command == '-b':
-            cmd_code = command_code_formatter('BRI')
-            data = str(sys.argv[2])
-            message = cmd_code + ':' + data
-        if command == '-d':
-            cmd_code = command_code_formatter('DAL')
-            data = str(sys.argv[2])
-            message = cmd_code + ':' + data
-        if command == '-c':
-            cmd_code = command_code_formatter('CDP')
-            data = str(sys.argv[2])
-            pwm = str(sys.argv[3])
-            message = cmd_code + ':' + data + ':' + pwm
-        if command == '-r':
-            cmd_code = command_code_formatter('TST')
-            data = str(sys.argv[2])
-            message =  data
-        if command == '-al':
-            cmd_code = command_code_formatter('ALM')
-            abs_or_rel = str(sys.argv[2])
-            sev = str(sys.argv[3])
-            data = str(sys.argv[4])
-            if len(sys.argv) > 5:
-                data = data + ':' + str(sys.argv[5])
-            if len(sys.argv) > 6:
-                data = data + ':' + str(sys.argv[6])
-            message = cmd_code + ':' + abs_or_rel + ':' + sev + ':' + data
-        if command == '-al?':
-            print(f'To set alarm: -al ABS_or_REL<a/r>* SEV TIME_STAMPS<000000-235959>* DURATION<1-n>* PERSISTANCE<0/1>.') 
-            print(f'Example: <-al a 0 1234 10 0>. * Are required. Defaults: PERSISTANCE: 0, DURATION: 10.')
-        if command == '-ald':
-            cmd_code = command_code_formatter('ALD')
-            abs_or_rel = str(sys.argv[2])
-            sev = str(sys.argv[3])
-            data = str(sys.argv[4])
-            message = cmd_code + ':' + abs_or_rel + ':' + sev + ':' + data
-
-        else:
-            logging.warning(f'Command not parsed: {sys.argv}')
-            print(f'Command not parsed: {sys.argv}')
-            
-
-        message = add_crc(message)
-        print(f"main:: message: {message}")
-        encrypted = encrypt(message)
-
-        hm = bt_ble_hm.BT_BLE_HM()
-        if hm.error == None:
-            hm.transmit(encrypted)
-            logging.info(f'Message sent: {message}, {encrypted}')
-            with open('bt_ble_command_counter.log', 'w') as f:
-                json.dump(command_counter, f)
-                #print(f'Updating command_counter.log: {command_counter} \n')
-        else:
-            logging.error(f'Message not sent: {message}, {encrypted} due to {hm.error}.')
-
-
-    else:
+    if len(sys.argv) <= 1:
         print(f'''
 
-        Missing argument for message to be sent.
+        Usage: {sys.argv[0]} options
             -a   (AIX000) | Absolute indexing. Args: start, stop, RGB
             -z   (ZIX000) | Zone indexing. Args: zone, RGB
             -g   (GRA000) | Gradient. Args: start, stop, RGB
@@ -191,8 +95,150 @@ def run_command():
             -r   (TST000) | Raw string.
             -e   (ENC000) | Raw string, encoded.
             -al  (ALM000) | Abs or Rel, severity, 
+            -ald (ALM000) | Abs or Rel, severity, 
 
             ''')
+        return -1
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
+        if command == '-a':
+            cmd_code = command_code_formatter('AIX')
+            start = str(sys.argv[2])
+            end = str(sys.argv[3])
+            data = str(sys.argv[4])
+            message = cmd_code + ':' + start + ':' + end + ':' + data
+            return message
+        if command == '-z':
+            cmd_code = command_code_formatter('ZIX')
+            zone = str(sys.argv[2])
+            data = str(sys.argv[3])
+            message = cmd_code + ':' + zone + ':' + data
+            return message
+        if command == '-g':
+            cmd_code = command_code_formatter('GRA')
+            data = ''
+            ds = sys.argv[2:]
+            for d in ds:
+                data = data + ':' + d
+            message = cmd_code + data
+            print(f'run_command:: cmd_code: {cmd_code}, message: {message}, data: {data}, ds: {ds}')
+            return message
+        if command == '-g?':
+            print(f'To set gradient: -g START<0-n>* RrGgBb<000000-ffffff>* MIDDLE_n0<0-n>* RrGgBb<000000-ffffff>* MIDDLE_n1<0-n>* RrGgBb<000000-ffffff>* END<0-n>* RrGgBb<000000-ffffff>*.') 
+            print(f'Example: <-g 0 00ff00 10 ff0000 20 0000ff>. * Are required. Any number of middle points are accepted so long there are as many RrGgBb values')
+            return -1
+        if command == '-t':
+            cmd_code = command_code_formatter('TSY')
+            data  = ticks_to_time()
+            message = cmd_code + ':' + data
+            return message
+        if command == '-b':
+            cmd_code = command_code_formatter('BRI')
+            data = str(sys.argv[2])
+            message = cmd_code + ':' + data
+            return message
+        if command == '-d':
+            cmd_code = command_code_formatter('DAL')
+            data = str(sys.argv[2])
+            message = cmd_code + ':' + data
+            return message
+        if command == '-c':
+            cmd_code = command_code_formatter('CDP')
+            data = str(sys.argv[2])
+            pwm = str(sys.argv[3])
+            message = cmd_code + ':' + data + ':' + pwm
+            return message
+
+        if command == '-sp':
+            cmd_code = command_code_formatter('SCI')
+            on_off = str(sys.argv[2])
+            if len(sys.argv) < 3:
+                freq = 1
+            else:
+                freq = str(sys.argv[3])
+            if len(sys.argv) < 4:
+                para = 0
+            else:
+                para = str(sys.argv[4])
+            message = cmd_code + ':' + on_off + ':' + freq + ':' + para
+            return message
 
 
-run_command()
+        if command == '-r':
+            cmd_code = command_code_formatter('TST')
+            data = str(sys.argv[2])
+            message =  data
+            return message
+        if command == '-al':
+            cmd_code = command_code_formatter('ALM')
+            abs_or_rel = str(sys.argv[2])
+            sev = str(sys.argv[3])
+            data = str(sys.argv[4])
+            if len(sys.argv) > 5:
+                data = data + ':' + str(sys.argv[5])
+            if len(sys.argv) > 6:
+                data = data + ':' + str(sys.argv[6])
+            message = cmd_code + ':' + abs_or_rel + ':' + sev + ':' + data
+            return message
+        if command == '-al?':
+            print(f'To set alarm: -al ABS_or_REL<a/r>* SEV TIME_STAMPS<000000-235959>* DURATION<1-n>* PERSISTANCE<0/1>.') 
+            print(f'Example: <-al a 0 1234 10 0>. * Are required. Defaults: DURATION: 10, PERSISTANCE: 0.')
+            return -1
+        if command == '-ald':
+            cmd_code = command_code_formatter('ALD')
+            abs_or_rel = str(sys.argv[2])
+            sev = str(sys.argv[3])
+            data = str(sys.argv[4])
+            message = cmd_code + ':' + abs_or_rel + ':' + sev + ':' + data
+            return message
+
+        else:
+            logging.warning(f'Command not parsed: {sys.argv}')
+            print(f'Command not parsed: {sys.argv}')
+            return -1
+            
+    
+def compose_message(message):
+    message = add_crc(message)
+    print(f"main:: message: {message}")
+    encrypted = encrypt(message)
+    return encrypted
+        
+
+def send_message(message, encrypted):
+    hm = bt_ble_hm.BT_BLE_HM()
+    if not hm.error:
+        hm.transmit(encrypted)
+        logging.info(f'Message sent: {message}, {encrypted}')
+        with open('bt_ble_command_counter.log', 'w') as f:
+            json.dump(command_counter, f)
+            #print(f'Updating command_counter.log: {command_counter} \n')
+        return 1
+    else:
+        logging.error(f'Message not sent: {message}, {encrypted} due to {hm.error}.')
+        return -1
+
+
+ 
+
+if __name__ == "__main__":
+    try:
+        with open(CMD_COUNTER_LOG, 'r') as f:
+            command_counter = json.loads(f.read())
+    except FileNotFoundError:
+        m_w = f"file '{CMD_COUNTER_LOG}' was not present so it was created. If client device has command counters that are non-zero commands will not be accepted on client side."
+        logging.warning(m_w)
+        print(f"WARNING: ", m_w)
+        command_counter = {}
+        command_list = ['AIX', 'ZIX', 'GRA', 'TSY', 'BRI', 'DAL', 'CDP', 'TST', 'ALM']
+        for c in command_list:
+            command_counter[c] = 0
+        with open('bt_ble_command_counter.log', 'w') as f:
+            json.dump(command_counter, f)
+    
+    
+    
+    message = run_command()
+    if message != -1:
+        encrypted = compose_message(message)
+        send_message(message, encrypted)
