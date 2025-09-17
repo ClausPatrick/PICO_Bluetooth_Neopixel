45fb40bf49244d8fbe0ee8759c2a621ed1c356d5
ClausPatrick <44665589+ClausPatrick@users.noreply.github.com>
2025-09-17 13:25:08 +0100

    Update bt_ble_hm.py
    Added logging

--- bt_ble_hm.py
+++ bt_ble_hm.py
@@ -2,20 +2,24 @@
 # Test script to push data to HM18 BLE module via RPI
 
 import pydbus
-import sys
-import re
+import sys 
+import re 
+import logging
+
+
+logname = str(sys.argv[0])[:-3] + '.log'
+logging.basicConfig(filename=logname,  level=logging.DEBUG, format='%(asctime)s %(levelname)s %(funcName)s:%(lineno)d %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
 
 class BT_BLE_HM():
     def __init__(self):
         self.dev_id = MAC_ADDRESS
-
         self.bluez_service = 'org.bluez'
         adapter_path = '/org/bluez/hci0'
         device_path = f"{adapter_path}/dev_{self.dev_id.replace(':', '_')}"
         self.bus = pydbus.SystemBus()
         self.adapter = self.bus.get(self.bluez_service, adapter_path)
         self.device = self.bus.get(self.bluez_service, device_path)
-
+        self._error = 0
 
     def connect(self):
         self.device.Connect()
@@ -28,28 +32,38 @@ class BT_BLE_HM():
                 if path.startswith(dev_path) and chr_uuid == uuid:
                     return path
 
-
         hm_uuid = "0000ffe1-0000-1000-8000-00805f9b34fb"
         self.char_path = get_characteristic_path(self.device._path, hm_uuid)
-
-        #btn = bus.get(bluez_service, char_path)
-        #print(btn.ReadValue({}))
-
         self.hm_path = get_characteristic_path(self.device._path, hm_uuid)
         self.hm_period = self.bus.get(self.bluez_service, self.hm_path)
-
+        print(f'BT_BLE_HM::: connect:: hm_period: {self.hm_period}')
 
     def transmit(self, data):
         self.connect()
         new_value = str(data).encode('ascii')
-        self.hm_period.WriteValue(new_value, {})
+        try:
+            self.hm_period.WriteValue(new_value, {})
+            self._error = 0
+        except AttributeError:
+            self._error = 'AttributeError'
+            e_m = "BT_BLE_HM::: transmit:: Failure to write: {new_value}, {self._error}:\n"
+            print(e_m)
+            logging.error(e_m)
+            #print(f'dir: {dir(new_value)}\n')
         self.device.Disconnect()
 
+    @property
+    def error(self):
+        return self._error
 
-
-def main():
+def main():        
     hm = BT_BLE_HM()
-    hm.transmit(12345678901234567890123456789012345678901234567890123456789012345678901234567890)
+    hm.transmit(1234)
+    
+
+# call main
+if __name__ == '__main__':
+    main()
 
 
 
